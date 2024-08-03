3692a3ef0689542c0a69390d992a8894611585fe
ShelpAm <shelpam@qq.com>
2024-08-03 22:14:14 +0800

    feat(graph): tarjan's scc algorithm

--- competitest/Luogu/unknown_contest/P1073 [NOIP2009 提高组] 最优贸易/main.cpp
+++ competitest/Luogu/unknown_contest/P1073 [NOIP2009 提高组] 最优贸易/main.cpp
@@ -1,6 +1,167 @@
-#pragma once
-#include "../templates/main.cpp"
+/*Problem: P1073 [NOIP2009 提高组] 最优贸易*/
+/*Contest: unknown_contest*/
+/*Judge: Luogu*/
+/*URL: https://www.luogu.com.cn/problem/P1073*/
+/*Start: Sat 03 Aug 2024 07:09:55 PM CST*/
+/*Author: ShelpAm*/
 
+#include <bits/stdc++.h>
+
+#ifdef __cpp_lib_ranges
+#include <ranges>
+#endif
+
+namespace {
+[[maybe_unused]] constexpr std::int_fast64_t mod998244353{998'244'353LL};
+[[maybe_unused]] constexpr std::int_fast64_t mod1e9p7{1'000'000'007LL};
+[[maybe_unused]] constexpr double eps{1e-8};
+template <typename T>
+[[maybe_unused]] constexpr T inf{std::numeric_limits<T>::max() / 2};
+
+#ifdef __cpp_concepts
+// Concepts.
+namespace shelpam::concepts {
+template <typename> struct is_pair_t : std::false_type {};
+template <typename T, typename U>
+struct is_pair_t<std::pair<T, U>> : std::true_type {};
+template <typename T>
+concept pair = is_pair_t<T>::value;
+template <typename> struct is_tuple_t : std::false_type {};
+template <typename... Ts>
+struct is_tuple_t<std::tuple<Ts...>> : std::true_type {};
+template <typename... Ts>
+concept tuple = is_tuple_t<Ts...>::value;
+} // namespace shelpam::concepts
+
+constexpr auto operator>>(auto &istream, auto &&t) -> std::istream &
+{
+  using T = std::remove_cvref_t<decltype(t)>;
+  static_assert(!shelpam::concepts::tuple<T>, "tuple: not implemented yet.\n");
+#ifdef __cpp_lib_ranges
+  if constexpr (std::ranges::range<T>) {
+    for (auto &ele : t) {
+      istream >> ele;
+    }
+  }
+#endif
+  else if constexpr (shelpam::concepts::pair<T>) {
+    istream >> t.first >> t.second;
+  }
+  else {
+    istream >> t;
+  }
+  return istream;
+}
+#endif
+#ifndef ONLINE_JUDGE
+#include "/home/shelpam/Documents/projects/competitive-programming/libs/debug.h"
+#else
+#define debug(...)
+#endif
+template <typename T, typename U>
+constexpr auto chmax(T &value, U const &other) noexcept -> bool
+{
+  if (value < other) {
+    value = other;
+    return true;
+  }
+  return false;
+}
+template <typename T, typename U>
+constexpr auto chmin(T &value, U const &other) noexcept -> bool
+{
+  if (value > other) {
+    value = other;
+    return true;
+  }
+  return false;
+}
+template <typename T> constexpr auto sum_of(T const &coll) noexcept
+{
+  return std::accumulate(coll.begin(), coll.end(), std::int_fast64_t{});
+}
+constexpr auto pow(int a, std::int_fast64_t b,
+                   std::uint_fast64_t p) noexcept = delete;
+template <typename T>
+constexpr auto pow(T a, std::int_fast64_t b, std::uint_fast64_t p) noexcept
+{
+  assert(b >= 0);
+  decltype(a) res{1};
+  while (b != 0) {
+    if ((b & 1) == 1) {
+      res = res * a % p;
+    }
+    a = a * a % p;
+    b >>= 1;
+  }
+  return res;
+}
+template <typename F>
+auto binary_search(F check, std::int_fast64_t ok, std::int_fast64_t ng,
+                   bool check_ok = true) -> std::int_fast64_t
+{
+  if (check_ok) {
+    assert(check(ok));
+  }
+  while (std::abs(ok - ng) > 1) {
+    auto const x{(ok + ng) / 2};
+    (check(x) ? ok : ng) = x;
+  }
+  return ok;
+}
+template <typename T> constexpr auto lsb(T i) -> T
+{
+  static_assert(std::is_signed_v<T>,
+                "lsb is implemented based on signed integers.");
+  return i & -i;
+}
+// i mustn't be 0
+template <typename T> constexpr auto count_leading_zeros(T const &i) -> int
+{
+  assert(i != 0);
+  if constexpr (std::is_same_v<T, unsigned long long>) {
+    return __builtin_clzll(i);
+  }
+  if constexpr (std::is_same_v<T, unsigned long>) {
+    return __builtin_clzl(i);
+  }
+  if constexpr (std::is_same_v<T, unsigned int>) {
+    return __builtin_clz(i);
+  }
+  /*static_assert(false, "Unsupported type");*/
+  static_assert(!std::is_signed_v<T>,
+                "msb is implemented based on unsigned integers");
+  return -1; // Unreachable.
+}
+// i mustn't be 0
+template <typename T> constexpr auto msb(T i) -> int
+{
+  return static_cast<int>(sizeof(T) * CHAR_BIT - 1 - count_leading_zeros(i));
+}
+[[maybe_unused]] auto gen_rand()
+{
+  static std::mt19937_64 rng(
+      std::chrono::steady_clock::now().time_since_epoch().count());
+  return rng();
+}
+void solve_case();
+} // namespace
+auto main() -> int
+{
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  constexpr auto my_precision{10};
+  std::cout << std::fixed << std::setprecision(my_precision);
+  int t{1};
+  /*std::cin >> t;*/
+  for (int i{}; i != t; ++i) {
+    solve_case();
+  }
+  return 0;
+}
+namespace {
+using i64 = std::int_fast64_t;
+using u64 = std::uint_fast64_t;
 namespace graph {
 constexpr std::int_fast64_t infinity{
     std::numeric_limits<std::int_fast64_t>::max() / 2};
@@ -472,20 +633,38 @@ private:
   std::vector<int> _lowlink;
   std::vector<int> _on_stack;
 };
-auto contract_edges(Graph const &g) -> Graph
+} // namespace graph
+void solve_case()
 {
-  auto const scc{std::move(Tarjan_scc(g).scc)};
-
-  std::vector<int> scc_id(g.adjacent.size());
-  Graph h(scc.size());
-  scc_id.resize(scc.size());
-  for (int i{}; i != scc.size(); ++i) {
-    for (auto const u : scc[i]) {
-      scc_id[u] = i;
+  int n, m;
+  std::cin >> n >> m;
+  std::vector<int> a(n);
+  std::cin >> a;
+  graph::Graph g(n);
+  for (int i{}; i != m; ++i) {
+    int u, v, z;
+    std::cin >> u >> v >> z;
+    g.add_edge(--u, --v, 1);
+    if (z == 2) {
+      g.add_edge(v, u, 1);
     }
   }
-  for (std::size_t i{}; i != scc.size(); ++i) {
-    for (auto const u : scc[i]) {
+
+  graph::Tarjan_scc scc(g);
+  graph::Graph h(scc.scc.size());
+  std::vector<int> scc_id(n);
+  std::vector<int> b(scc.scc.size(), inf<int>);
+  std::vector<int> c(scc.scc.size(), -inf<int>);
+  for (int i{}; i != scc.scc.size(); ++i) {
+    for (auto const u : scc.scc[i]) {
+      scc_id[u] = i;
+      chmin(b[i], a[u]);
+      chmax(c[i], a[u]);
+    }
+  }
+  std::vector<int> in(scc.scc.size());
+  for (int i{}; i != scc.scc.size(); ++i) {
+    for (auto const u : scc.scc[i]) {
       for (auto const &[_, v] : g.edges_of(u)) {
         if (i != scc_id[v]) {
           h.add_edge(i, scc_id[v], 1);
@@ -493,6 +672,28 @@ auto contract_edges(Graph const &g) -> Graph
       }
     }
   }
-  return h;
+
+  int ans{};
+  std::vector<int> max_reachable(scc.scc.size(), -1);
+  std::vector<int> reachable_n(scc.scc.size());
+  reachable_n[scc_id[n - 1]] = 1;
+  std::function<void(int)> dfs{[&](int const u) {
+    if (max_reachable[u] != -1) {
+      return;
+    }
+    max_reachable[u] = c[u];
+    for (auto const &[_, v] : h.edges_of(u)) {
+      dfs(v);
+      if (reachable_n[v]) {
+        reachable_n[u] = 1;
+        chmax(max_reachable[u], max_reachable[v]);
+      }
+    }
+    if (reachable_n[u]) {
+      chmax(ans, max_reachable[u] - b[u]);
+    }
+  }};
+  dfs(scc_id[0]);
+  std::cout << ans << '\n';
 }
-} // namespace graph
+} // namespace
---
+++ competitest/Luogu/unknown_contest/P2002 消息扩散/main.cpp
@@ -0,0 +1,169 @@
+/*Problem: P2002 消息扩散*/
+/*Contest: unknown_contest*/
+/*Judge: Luogu*/
+/*URL: https://www.luogu.com.cn/problem/P2002*/
+/*Start: Sat 03 Aug 2024 10:12:00 PM CST*/
+/*Author: ShelpAm*/
+
+#include <bits/stdc++.h>
+
+#ifdef __cpp_lib_ranges
+#include <ranges>
+#endif
+
+namespace {
+[[maybe_unused]] constexpr std::int_fast64_t mod998244353{998'244'353LL};
+[[maybe_unused]] constexpr std::int_fast64_t mod1e9p7{1'000'000'007LL};
+[[maybe_unused]] constexpr double eps{1e-8};
+template <typename T>
+[[maybe_unused]] constexpr T inf{std::numeric_limits<T>::max() / 2};
+
+#ifdef __cpp_concepts
+// Concepts.
+namespace shelpam::concepts {
+template <typename> struct is_pair_t : std::false_type {};
+template <typename T, typename U>
+struct is_pair_t<std::pair<T, U>> : std::true_type {};
+template <typename T>
+concept pair = is_pair_t<T>::value;
+template <typename> struct is_tuple_t : std::false_type {};
+template <typename... Ts>
+struct is_tuple_t<std::tuple<Ts...>> : std::true_type {};
+template <typename... Ts>
+concept tuple = is_tuple_t<Ts...>::value;
+} // namespace shelpam::concepts
+
+constexpr auto operator>>(auto &istream, auto &&t) -> std::istream &
+{
+  using T = std::remove_cvref_t<decltype(t)>;
+  static_assert(!shelpam::concepts::tuple<T>, "tuple: not implemented yet.\n");
+#ifdef __cpp_lib_ranges
+  if constexpr (std::ranges::range<T>) {
+    for (auto &ele : t) {
+      istream >> ele;
+    }
+  }
+#endif
+  else if constexpr (shelpam::concepts::pair<T>) {
+    istream >> t.first >> t.second;
+  }
+  else {
+    istream >> t;
+  }
+  return istream;
+}
+#endif
+#ifndef ONLINE_JUDGE
+#include "/home/shelpam/Documents/projects/competitive-programming/libs/debug.h"
+#else
+#define debug(...)
+#endif
+template <typename T, typename U>
+constexpr auto chmax(T &value, U const &other) noexcept -> bool
+{
+  if (value < other) {
+    value = other;
+    return true;
+  }
+  return false;
+}
+template <typename T, typename U>
+constexpr auto chmin(T &value, U const &other) noexcept -> bool
+{
+  if (value > other) {
+    value = other;
+    return true;
+  }
+  return false;
+}
+template <typename T> constexpr auto sum_of(T const &coll) noexcept
+{
+  return std::accumulate(coll.begin(), coll.end(), std::int_fast64_t{});
+}
+constexpr auto pow(int a, std::int_fast64_t b,
+                   std::uint_fast64_t p) noexcept = delete;
+template <typename T>
+constexpr auto pow(T a, std::int_fast64_t b, std::uint_fast64_t p) noexcept
+{
+  assert(b >= 0);
+  decltype(a) res{1};
+  while (b != 0) {
+    if ((b & 1) == 1) {
+      res = res * a % p;
+    }
+    a = a * a % p;
+    b >>= 1;
+  }
+  return res;
+}
+template <typename F>
+auto binary_search(F check, std::int_fast64_t ok, std::int_fast64_t ng,
+                   bool check_ok = true) -> std::int_fast64_t
+{
+  if (check_ok) {
+    assert(check(ok));
+  }
+  while (std::abs(ok - ng) > 1) {
+    auto const x{(ok + ng) / 2};
+    (check(x) ? ok : ng) = x;
+  }
+  return ok;
+}
+template <typename T> constexpr auto lsb(T i) -> T
+{
+  static_assert(std::is_signed_v<T>,
+                "lsb is implemented based on signed integers.");
+  return i & -i;
+}
+// i mustn't be 0
+template <typename T> constexpr auto count_leading_zeros(T const &i) -> int
+{
+  assert(i != 0);
+  if constexpr (std::is_same_v<T, unsigned long long>) {
+    return __builtin_clzll(i);
+  }
+  if constexpr (std::is_same_v<T, unsigned long>) {
+    return __builtin_clzl(i);
+  }
+  if constexpr (std::is_same_v<T, unsigned int>) {
+    return __builtin_clz(i);
+  }
+  /*static_assert(false, "Unsupported type");*/
+  static_assert(!std::is_signed_v<T>,
+                "msb is implemented based on unsigned integers");
+  return -1; // Unreachable.
+}
+// i mustn't be 0
+template <typename T> constexpr auto msb(T i) -> int
+{
+  return static_cast<int>(sizeof(T) * CHAR_BIT - 1 - count_leading_zeros(i));
+}
+[[maybe_unused]] auto gen_rand()
+{
+  static std::mt19937_64 rng(
+      std::chrono::steady_clock::now().time_since_epoch().count());
+  return rng();
+}
+void solve_case();
+} // namespace
+auto main() -> int
+{
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  constexpr auto my_precision{10};
+  std::cout << std::fixed << std::setprecision(my_precision);
+  int t{1};
+  /*std::cin >> t;*/
+  for (int i{}; i != t; ++i) {
+    solve_case();
+  }
+  return 0;
+}
+namespace {
+using i64 = std::int_fast64_t;
+using u64 = std::uint_fast64_t;
+void solve_case()
+{
+  /*return;*/
+}
+} // namespace
